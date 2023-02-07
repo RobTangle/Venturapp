4a9917949df21ccbda5ee573ce07fe37a60309e0
Robert0101 <rob.tangled@gmail.com>
2023-02-07 16:11:53 -0300

    setup config object for PORT and database uri

---
+++ api/src/config/index.ts
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const PORT = process.env.PORT;
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+
+const config = {
+  server: {
+    port: PORT,
+  },
+  database: {
+    uri: MONGO_DB_URI,
+  },
+};
+
+export default config;
