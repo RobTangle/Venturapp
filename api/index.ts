4b5ae0ea7e3bd8c6bfb207b37cff85cdee144283
Robert0101 <rob.tangled@gmail.com>
2023-02-08 10:32:20 -0300

    Set database connection when the server starts running

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,22 @@
+import mongoose from "mongoose";
 import app from "./src/app";
 import config from "./src/config";
+import LogWithColors from "./src/miscellanea/LogWithColor";
 
 const PORT = config.server.port || 3333;
+const MONGO_URI = config.database.uri;
 
 app.listen(PORT, () => {
-  console.log(`**************************************************`);
-  console.log(`******* Server listening on port ${PORT} *******`);
-  console.log(`**************************************************`);
+  LogWithColors.info(`******* Server listening on port ${PORT} *******`);
+
+  console.log("*** Connecting to database........ ");
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      LogWithColors.info("******* Connected to MongoDB ******* ");
+    })
+    .catch((error) => {
+      LogWithColors.error(`*** Unable to connect to MongoDB ***`);
+      console.log({ error });
+    });
 });
