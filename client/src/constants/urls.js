f741d48d962acd6eeb67d27743d1abaddc8b823c
Robert0101 <rob.tangled@gmail.com>
2023-02-08 21:41:04 -0300

    comment console.log of URL

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,5 +1,5 @@
 export const URL =
   process.env.REACT_APP_BACKEND_URL || "http://localhost:3333/";
-console.log({ url: URL });
+// console.log({ url: URL });
 export const URL_GET_TRIPS = URL + "trips";
 export const URL_POST_TRIP = URL + "trips";
